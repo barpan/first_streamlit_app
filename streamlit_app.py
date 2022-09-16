22aaa079ac64bf74455dcd21d5e035cdc3719471
barpan <barpan@gmail.com>
2022-09-15 23:21:03 -0500

    Update streamlit_app.py
    Added a multi-select control to pick up fruits from the table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Adding a pick-up list so guests can pick the fruit they want for their smoothie.
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#Display the table on the page
 streamlit.dataframe(my_fruit_list)
